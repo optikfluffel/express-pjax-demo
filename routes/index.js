91c565ca4f56bd2062ac1b1470259d04f4411408
Udo Kramer <info@udokramer.com>
2013-03-10 11:58:20 +0100

    just add some smicolons to stop the linter from beeing grumpy

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@
  */
 
 exports.index = function(req, res){
-  res.renderPjax('index', { title: 'home' })
+  res.renderPjax('index', { title: 'home' });
 };
 
 exports.dinosaurs = function(req, res){
-  res.renderPjax('dinosaurs', { title: 'dinosaurs' })
+  res.renderPjax('dinosaurs', { title: 'dinosaurs' });
 };
 
 exports.aliens = function(req, res){
-  res.renderPjax('aliens', { title: 'aliens' })
+  res.renderPjax('aliens', { title: 'aliens' });
 };
