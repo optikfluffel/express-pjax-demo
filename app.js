4cf0b04948b8af37dbb26d1db819db6238837f2d
Udo Kramer <info@udokramer.com>
2013-03-10 10:30:03 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-
 /**
  * Module dependencies.
  */
+var express = require('express'),
+    pjax = require('express-pjax'),
+    routes = require('./routes');
 
-var express = require('express')
-  , pjax = require('express-pjax')
-  , routes = require('./routes')
+var app = express();
 
-var app = module.exports = express.createServer();
+// Define port
+app.port = process.env.PORT || 3000;
 
-// Configuration
-
+// App configuration
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(pjax());
   app.use(express.bodyParser());
-  app.use(express.methodOverride());
+  //app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
+// Confiure error handling
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
-
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 // Routes
-
 app.get('/', routes.index);
 app.get('/dinosaurs', routes.dinosaurs);
 app.get('/aliens', routes.aliens);
 
+// Start server
 app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+console.log("Express server listening on port %s in %s mode", app.port, app.settings.env);
